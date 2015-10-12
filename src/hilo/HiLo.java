b3fb2107e3e21db2e56ad157621fb9ebcf2256a4
l1onh3art88 <spenccheng@gmail.com>
2015-10-12 11:00:08 -0500

    HiLo

--- src/hilo/HiLo.java
+++ src/hilo/HiLo.java
@@ -20,63 +20,61 @@ public class HiLo {
     public static void main(String[] args) {
         // TODO code application logic here
         Random random = new Random();
-        int randNum = random.nextInt(100);
-        final int BreakNum = 420;
-        boolean playGame = true;
+        final int BreakNum = 0;
         String replay;
-        int count = 0;
-        System.out.println("Guess the correct number between 1 and 100");
+        replay = "Y";
         Scanner scan = new Scanner(System.in);
-        int x = scan.nextInt();
-        while (true){
-         if( x == BreakNum){
+        int x;
+        int count;
+        do{
+                   count = 0;
 
-            count++;
-            replay = scan.nextLine();
-            System.out.println("You lost the game. Want to play again?\t (Y/N)");
-            if(replay.equals("Y")){
-                randNum = random.nextInt(100);
-                System.out.println("Guess the correct number between 1 and 100");
-                x = scan.nextInt();
-                count = 0;
+        int randNum = random.nextInt(100);
+        System.out.println("Guess the correct number between 1 and 100; \'0\' to quit");
+                   x = scan.nextInt();
+        // Starts the game and asks for a random number
+        do{    
+          if(x>0){
+            if(x==randNum){
+                //Checks if the guess is = to the answer and counts the guesses
+                count++;
+                System.out.println("You guessed " + count + " times");
+                System.out.println("You won the game!");
+                
+
             }
-            else if(replay.equals("N")){
-                System.out.println("The game has ended");
-                break;
+
+            else if(x < randNum){
+                //Checks if guess is below the generated number
+              System.out.println("Your guess is too low. Try again!\n Type \'0\' to quit");
+              x = scan.nextInt();
+              count++;
+             }
+            else {
+                //Checks if guess is above the generated number
+              System.out.println("Your guess is too high. Try again!\n Type \'0\' to quit");
+              x = scan.nextInt();
+              count++;
             }
-            
-         }
-          else if(x < randNum){
-            System.out.println("Your guess is too low. Try again!\n Type \'420\' to quit");
-            x = scan.nextInt();
-            count++;
-           }
-          else if(x > randNum){
-            System.out.println("Your guess is too high. Try again!\n Type \'420\' to quit");
-            x = scan.nextInt();
-            count++;
           }
-       
-        else if(x == randNum){
-            replay = scan.nextLine();
-            count++;
-            System.out.println("You guessed " + count + " times.");
-            System.out.println("You won the game! Want to play again? \t (Y/N)");
-            if(replay.equals("Y")){
-                randNum = random.nextInt(100);
-                System.out.println("Guess the correct number between 1 and 100");
-                x = scan.nextInt();
-                count = 0;
-            }
-            else if(replay.equals("N")){
-                System.out.println("The game has ended");
-                break;
-            }
         }
-        else{
-            System.out.println("Not a valid option");
+        while(x!= BreakNum && x!= randNum);
+        //Opens prompt to play game again after winning or losing
+        if(x==randNum){
+            System.out.println("You guessed " + count + " times");
+            System.out.println("Play again? (Y/N)");
+            replay = scan.next();
         }
-       
-      }
+        else if(x==BreakNum){
+            System.out.println("You lost");
+            System.out.println("Play again? (Y/N)");
+            replay = scan.next();
+
+        }
+        
+        }
+        //Replays game if answer to prompt is Yes
+        while(replay.equalsIgnoreCase("Y"));
+    
     }    
 }
